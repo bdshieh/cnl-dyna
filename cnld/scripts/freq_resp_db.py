b316810f1c00809204b7c72f1e8192b4c24e1977
bernie <bsauce@gmail.com>
2018-12-17 11:33:55 +0000

    ~

--- cnld/scripts/freq_resp_db.py
+++ cnld/scripts/freq_resp_db.py
@@ -48,7 +48,7 @@ def process(job):
     # create Z matrix in HFormat
     hmkwrds = ['aprx', 'basis', 'admis', 'eta', 'eps', 'm', 'clf', 'eps_aca', 'rk', 'q_reg', 'q_sing', 'strict']
     hmargs = { k:getattr(cfg, k) for k in hmkwrds }
-    Z = bem.z_from_abstract(array, k, refn, **hmargs)
+    # Z = bem.z_from_abstract(array, k, refn, **hmargs)
 
     # construct G in HFormat from MBK and Z
     # G = MBK + (2 * np.pi * f) ** 2 * -1000. * Z 
