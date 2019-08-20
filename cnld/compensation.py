4c180f150a68134d1fcdab62a0add77bcdc7ca68
bernie <bsauce@gmail.com>
2019-08-20 17:03:38 +0100

    ~

--- cnld/compensation.py
+++ cnld/compensation.py
@@ -374,12 +374,108 @@ def mem_patch_comp_funcs(mem, refn, lmbd, k, n, x0=None):
     return comp_funcs
 
 
-# def make_fcomp(fcomp1, fcomp2):
-#     def fcomp(x, v):
-#         return fcomp1(x) * v**2 + fcomp2(x)
-#     fcomp.fcomp1 = fcomp1
-#     fcomp.fcomp2 = fcomp2
-#     return fcomp
+@util.memoize
+def mem_static_x_vector(mem, refn, vdc, k, x0, atol=1e-10, maxiter=100):
+    '''
+    '''
+    def pes(v, x, g_eff):
+        return -e_0 / 2 * v ** 2 / (g_eff + x) ** 2
+
+    if isinstance(mem, abstract.SquareCmutMembrane):
+        amesh = mesh.square(mem.length_x, mem.length_y, refn)
+    else:
+        amesh = mesh.circle(mem.radius, refn)
+
+    K = fem.mem_k_matrix(mem, refn)
+    g_eff = mem.gap + mem.isolation / mem.permittivity
+    F = fem.mem_f_vector(mem, refn, 1)
+    Kinv = np.linalg.inv(K)
+    nnodes = K.shape[0]
+    x = np.zeros(nnodes)
+
+    for i in range(maxiter):
+        p = F * pes(vdc, x, g_eff)
+        p[x < x0] += -k * (x[x < x0] - x0) * F[x < x0]
+
+        xnew = Kinv.dot(p)
+
+        if np.max(np.abs(xnew - x)) < atol:
+            is_collapsed = False
+            return xnew, is_collapsed
+
+        x = xnew
+
+    is_collapsed = True
+    return x, p, is_collapsed
+
+    
+@util.memoize
+def mem_patch_comp_funcs(mem, refn, lmbd, k, n, x0=None):
+    '''
+    Create compensation functions for each patch.
+    '''
+    f = fem.mem_patch_f_matrix(mem, refn)
+    avg = fem.mem_patch_avg_matrix(mem, refn)
+
+    K = fem.mem_k_matrix(mem, refn)
+    Kinv = fem.inv_block(K)
+
+    g = mem.gap
+    g_eff = mem.gap + mem.isolation / mem.permittivity
+
+    xprof = Kinv.dot(-np.sum(f, axis=1)).squeeze()
+    xprof = xprof / np.max(np.abs(xprof))
+
+    comp_funcs = []
+
+    for i, pat in enumerate(mem.patches):
+
+        avg_pat = avg[:, i]
+        xnorm = xprof / np.max(np.abs(xprof[avg_pat > 0]))
+
+        if x0 is None:
+            x0 = (xnorm * g).dot(avg_pat)
+
+        p_es = []
+        xavg = []
+        xmax = []
+
+        for i, d in enumerate(np.linspace(-2, 2, 81)):
+
+            x = xnorm * g * d
+            x[x < -g] = -g
+
+            _xavg = x.dot(avg_pat)
+            _xmax = np.abs(x[avg_pat > 0]).max() * np.sign(-d)
+            _p_es = -e_0 / 2 / (x + g_eff)**2
+
+            if i > 0:
+                if _xavg == xavg[i - 1]:
+                    break
+
+            xavg.append(_xavg)
+            xmax.append(_xmax)
+            p_es.append(_p_es.dot(avg_pat))
+
+        p_es = np.array(p_es)[::-1]
+        xavg = np.array(xavg)[::-1]
+        xmax = np.array(xmax)[::-1]
+
+        cs_p_es = CubicSpline(xavg, p_es, extrapolate=False)
+        p_es_func = make_p_es_func(np.min(xavg), np.min(p_es), cs_p_es)
+
+        cs_xmax = CubicSpline(xavg, xmax, extrapolate=False)
+        xmax_func = make_xmax_func(np.min(xavg), np.min(xmax), cs_xmax)
+
+        p_cont_spr_func = make_p_cont_spr_func(k, n, x0)
+
+        p_cont_dmp_func = make_p_cont_dmp_func(lmbd, n, x0)
+
+        comp_funcs.append({'p_es': p_es_func, 'xmax': xmax_func,
+                           'p_cont_spr': p_cont_spr_func,
+                           'p_cont_dmp': p_cont_dmp_func})
+
+    return comp_funcs
 
 
 def array_patch_comp_funcs(array, refn, lmbd, k, n, **kwargs):
